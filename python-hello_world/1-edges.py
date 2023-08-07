44aeb01ba8d487b0425619259a0a8e477cfc78b0
Kanemwills <udohprincewill144@gmail.com>
2023-08-07 22:13:15 +0100

    copy-cut-paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,11 @@
+word = "Holberton"
+
+#Declare the variables
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+
+#Print results
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
