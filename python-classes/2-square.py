fcc2860e1cda932d0ee55e3f3e345be41e1db455
Kanemwills <udohprincewill144@gmail.com>
2023-08-09 00:45:08 +0100

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,3 +8,24 @@ class squares:
 
     def area(self):
         return self.__size ** 2
+
+Square = __import__('2-square').Square
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e) 
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)           
+
+my_square_2 = Square(5) 
+print("Area: {}".format(my_square_2.area()))   
+
+
+
