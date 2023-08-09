23f37827a2db3691aa34d544835031ae817c2e3f
Kanemwills <udohprincewill144@gmail.com>
2023-08-09 02:15:54 +0100

    Printing a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+"""An square class with a private object attribute"""
 class square:
     def __init__(self, size):
         self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class squares:
     def __init__(self, size=0):
         if not isinstance(size, int):
@@ -6,26 +10,7 @@ class squares:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
+"""Returns the area of the square"""       
+
+def area(self):
         return self.__size ** 2
-
-Square = __import__('2-square').Square
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e) 
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)           
-
-my_square_2 = Square(5) 
-print("Area: {}".format(my_square_2.area()))   
-
-
-
