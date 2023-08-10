e509c2607530f92a685455dfee865999a6150231
Kanemwills <udohprincewill144@gmail.com>
2023-08-10 03:17:15 +0100

    Add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,7 @@
-def add(a, b):
-    return a + b
-
-a = 1
-b = 2
-from add_0 import add
-
-#print result
-
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    
