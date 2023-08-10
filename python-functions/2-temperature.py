da2f2da091b7d53df6eebb49ac405774ed1a62e1
Kanemwills <udohprincewill144@gmail.com>
2023-08-10 03:11:26 +0100

    Temperature

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,8 +1,5 @@
+#!/usr/bin/python3
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5 / 9
+    celsius = (fahrenheit - 32) * 5/9
     return celsius
 
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
