444ffd38766616fc5ad9563c8857a58c9ec2d897
Kanemwills <udohprincewill144@gmail.com>
2023-08-11 00:22:01 +0100

    Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,28 +5,14 @@
 - of the X-Request-Id variable found in the header ofthe response.
 """
 
-import requests
+
 import sys
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
-    url = sys.argv[1]
-
-    try:
-        response = requests.get(url)
-        x_request_id = response.headers.get('X-Request-Id')
-
-        if x_request_id is not None:
-            print(f"X-Request-Id: {x_request_id}")
-        else:
-            print("X-Request-Id header not found in the response.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
+import urllib.request
 
 if __name__ == "__main__":
-    main()
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
+        
