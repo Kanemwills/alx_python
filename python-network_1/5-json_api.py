03e3848cb3537bc339e46164cc64df0cfdfeae8b
Kanemwills <udohprincewill144@gmail.com>
2023-08-11 00:48:13 +0100

    Search API

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -4,31 +4,21 @@
 - sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
-
-import requests
 import sys
+import requests
 
-def main():
-    letter = input("Enter a letter: ")
-    url = "http://0.0.0.0:5000/search_user"
-
-    params = {'q': letter}
-
-    try:
-        response = requests.post(url, data=params)
-        response_data = response.json()
-
-        if isinstance(response_data, dict) and 'id' in response_data and 'name' in response_data:
-            print(f"[{response_data['id']}] {response_data['name']}")
-        elif isinstance(response_data, list) and len(response_data) > 0:
-            print(f"[{response_data[0]['id']}] {response_data[0]['name']}")
-        elif response_data == {}:
-            print("No result")
-        else:
-            print("Not a valid JSON")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
+        
