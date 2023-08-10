a965ca858c9ee218c7ce8ef79e632c29a05d3077
Kanemwills <udohprincewill144@gmail.com>
2023-08-10 13:04:27 +0100

    Display#1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,11 @@ class Rectangle(Base):
     
     def display(self):
         """Print the Rectangle using the `#` character."""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+    
