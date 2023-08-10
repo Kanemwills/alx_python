7565afff971aae4dda9d6f25fa8b78d971c9cf2f
Kanemwills <udohprincewill144@gmail.com>
2023-08-11 00:30:08 +0100

    push

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,13 +6,23 @@
 """
 
 
+import requests
 import sys
-import urllib.request
+
+def main():
+    url = input("Enter the URL: ")
+
+    try:
+        response = requests.get(url)
+        x_request_id = response.headers.get('X-Request-Id')
+
+        if x_request_id is not None:
+            print(f"X-Request-Id: {x_request_id}")
+        else:
+            print("X-Request-Id header not found in the response.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
-        
+    main()
