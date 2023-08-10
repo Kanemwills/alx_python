dacadff466b09e7913460909ba59bd32977ec38c
Kanemwills <udohprincewill144@gmail.com>
2023-08-10 02:49:32 +0100

    power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,15 +1,5 @@
+#!/usr/bin/python3
+# Author -Godswill Kalu
+
 def pow(a, b):
-    result = 1
-    if b >= 0:
-        for _ in range(b):
-            result *= a
-    else:
-        for _ in range(-b):
-            result /=a
-    return result  
-      
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))      
+    return (a ** b)
