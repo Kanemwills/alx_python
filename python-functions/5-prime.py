958ca7fa8c582f3cd33edcce72420fadb3e4d8cc
Kanemwills <udohprincewill144@gmail.com>
2023-08-10 03:01:27 +0100

    Prime

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,13 +1,19 @@
+#!/usr/bin/python3
 def is_prime(number):
-    if number < 2:
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+
+    if number % 2 == 0 or number % 3 == 0:
         return False
 
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
             return False
+        i += 6
 
     return True
-print(is_prime(17))
-print(is_prime(15))
-print(is_prime(-5))
-print(is_prime(0))
+
+
