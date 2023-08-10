0c791b5c9c5523b9f43da973b8dacad5ec88b3cc
Kanemwills <udohprincewill144@gmail.com>
2023-08-10 01:50:11 +0100

    correction

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,24 +2,7 @@
 """Defines a class Square"""
 
 
-"""An square class with a private object attribute"""
+"""Represent a square."""
 class Square:
-    def __init__(self, size=0):
-        self.__size = size
-        
-Square = __import__('0-square').Square
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
-       
+    pass
+    
