70e82670d1a163244982737cf31dbc309ce5dadd
Kanemwills <udohprincewill144@gmail.com>
2023-08-11 00:52:10 +0100

    Error code #1

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
 """A script that:
 - takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
 """
 
-import requests
-import sys
-
-def main():
-    url = input("Enter the URL: ")
-
-    try:
-        response = requests.get(url)
-        response_body = response.text
-
-        print("Response body:")
-        print(response_body)
-
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
+        
