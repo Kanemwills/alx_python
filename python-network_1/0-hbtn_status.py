a821a21d8e9e106fcbc3cec8efd8e7b15569da4b
Kanemwills <udohprincewill144@gmail.com>
2023-08-30 01:04:53 +0100

    push

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
-def main():
-    url = "https://alx-intranet.hbtn.io/status"
-
-    try:
-        response = requests.get(url)
-        response_data = response.json()
-
-        print("Body response:")
-        print(f"\t- type: {response_data['__class__']}")
-        print(f"\t- content: {response_data['content']}")
-        print(f"\t- utf8 content: {response_data['utf8content']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
-
-
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
+    
