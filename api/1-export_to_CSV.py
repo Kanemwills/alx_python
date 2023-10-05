8995528236c49513c76d13eea8f1669c3939e7fb
Princewill Udoh <udohprincewill144@gmail.com>
2023-10-06 00:50:28 +0100

    push

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,37 +1,36 @@
-import csv
 import requests
+import csv
 import sys
 
-if __name__ == '__main__':
+def get_employee_data(employee_id):
+    employee_info = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    todos = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
 
-    id = sys.argv[1]
-    todo = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-    employee = f"https://jsonplaceholder.typicode.com/users/{id}"
+    if employee_info.status_code == 200 and todos.status_code == 200:
+        employee_data = employee_info.json()
+        todos_data = todos.json()
 
-    #employee name
-    response = requests.get(employee)
-    data = response.json()
-    employee_name = data['name']
-
-    #todo list
-    todo_result = requests.get(todo)
-    todo_data = todo_result.json()
-    num_of_done_tasks = sum(1 for task in todo_data if task['completed'] )
-    total_num_task = len(todo_data)
-
-    print(f"Employee {employee_name} is done with tasks({num_of_done_tasks}/{total_num_task}):")
-
-    for task in todo_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
-
-
-    with open(f'{id}.csv', mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        completed_tasks = [task for task in todos_data if task['completed']]
         
-        for task in todo_data:
-            csv_writer.writerow([id, employee_name, task['completed']])
+        # Export data to CSV
+        csv_filename = f"{employee_data['id']}.csv"
+        with open(csv_filename, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-    print(f"Data has been exported to {csv_file}")
-    
+            for task in completed_tasks:
+                csv_writer.writerow([employee_data['id'], employee_data['username'], "Completed", task['title']])
+
+        print(f"Data exported to {csv_filename} successfully.")
+    else:
+        print("Error: Unable to fetch data.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+    else:
+        try:
+            employee_id = int(sys.argv[1])
+            get_employee_data(employee_id)
+        except ValueError:
+            print("Error: Invalid employee ID. Please provide a valid integer.")
