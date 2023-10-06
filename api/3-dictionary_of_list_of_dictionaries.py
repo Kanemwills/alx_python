f0742ca58110947e468fa6f28729831eaa4a1258
Princewill Udoh <udohprincewill144@gmail.com>
2023-10-06 01:09:50 +0100

    push

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,37 +1,35 @@
-#!/usr/bin/python3
-import json
 import requests
-import sys
+import json
+
+def get_all_employees_data():
+    all_employees_data = {}
+
+    for employee_id in range(1, 11):  # Assuming employee IDs range from 1 to 10
+        employee_info = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+        todos = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+
+        if employee_info.status_code == 200 and todos.status_code == 200:
+            employee_data = employee_info.json()
+            todos_data = todos.json()
+
+            tasks = []
+            for task in todos_data:
+                tasks.append({
+                    "username": employee_data["username"],
+                    "task": task["title"],
+                    "completed": task["completed"]
+                })
+
+            all_employees_data[employee_data["id"]] = tasks
+        else:
+            print(f"Error: Unable to fetch data for employee ID {employee_id}")
+
+    # Export data to JSON
+    json_filename = "todo_all_employees.json"
+    with open(json_filename, 'w') as jsonfile:
+        json.dump(all_employees_data, jsonfile, indent=4)
+
+    print(f"Data exported to {json_filename} successfully.")
 
 if __name__ == "__main__":
-    # API endpoint for fetching user data
-    users_url = 'https://jsonplaceholder.typicode.com/users'
-
-    # Get all users
-    users_response = requests.get(users_url)
-    users_data = users_response.json()
-
-    # Initialize a dictionary to store tasks for all users
-    tasks_dict = {}
-
-    # Loop through users and fetch their tasks
-    for user in users_data:
-        user_id = str(user['id'])
-        username = user['username']
-
-        # API endpoint for fetching tasks for a specific user
-        tasks_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
-        tasks_response = requests.get(tasks_url)
-        tasks_data = tasks_response.json()
-
-        # Create a list of task dictionaries for the current user
-        user_tasks = [{'username': username, 'task': task['title'], 'completed': task['completed']} for task in tasks_data]
-
-        # Add the user's tasks to the tasks_dict
-        tasks_dict[user_id] = user_tasks
-
-    # Write the tasks_dict to a JSON file
-    output_file = 'todo_all_employees.json'
-    with open(output_file, 'w') as json_file:
-        json.dump(tasks_dict, json_file)
-        
+    get_all_employees_data()
