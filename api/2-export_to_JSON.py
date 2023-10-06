65fb2d22e72389cfa017b9872275b7fedda661e3
Princewill Udoh <udohprincewill144@gmail.com>
2023-10-06 01:03:07 +0100

    push

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,81 +1,49 @@
-
-"""
-    This module fetches employee information
-    and their to-do list from a REST API and saves it in a JSON file.
-"""
-import json
 import requests
+import json
 import sys
 
-# Define a function to fetch employee information
-def get_employee_info(employee_id):
-    """
-    Fetches employee information from the API.
+def get_employee_data(employee_id):
+    # Define API endpoints
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/users/{employee_id}/todos"
 
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        dict: A dictionary containing employee information.
-    """
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    # Fetch employee data
     response = requests.get(employee_url)
-    data = response.json()
-    return data
+    employee_data = response.json()
+    user_id = employee_data.get("id")
+    username = employee_data.get("username")
 
-# Define a function to fetch the to-do list of an employee
-def get_todo_list(employee_id):
-    """
-    Fetches the to-do list of an employee from the API.
+    # Fetch TODO list data
+    response = requests.get(todos_url)
+    todos_data = response.json()
 
-    Args:
-        employee_id (int): The ID of the employee.
+    return user_id, username, todos_data
 
-    Returns:
-        list: A list of to-do tasks for the employee.
-    """
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    response = requests.get(todo_url)
-    todo_data = response.json()
-    return todo_data
+def export_to_json(employee_id):
+    user_id, username, todos_data = get_employee_data(employee_id)
 
-# Define a function to process and save employee data to a JSON file
-def save_employee_data_to_json(employee_id):
-    """
-    Fetches employee information and to-do list, processes the data,
-    and saves it to a JSON file.
+    json_filename = f"{user_id}.json"
 
-    Args:
-        employee_id (int): The ID of the employee.
-    """
-    employee_info = get_employee_info(employee_id)
-    employee_name = employee_info['name']
+    data_to_export = {str(user_id): [{"task": todo["title"], "completed": todo["completed"], "username": username} for todo in todos_data]}
 
-    todo_list = get_todo_list(employee_id)
-    num_of_done_tasks = sum(1 for task in todo_list if task['completed'])
-    total_num_tasks = len(todo_list)
+    with open(json_filename, mode='w') as json_file:
+        json.dump(data_to_export, json_file, indent=4)
 
-    print(f"Employee {employee_name} is done with tasks ({num_of_done_tasks}/{total_num_tasks}):")
-
-    json_data = {
-        employee_id: [
-            {
-                "task": task["title"],
-                "completed": task["completed"],
-                "username": employee_name
-            }
-            for task in todo_list
-        ]
-    }
-
-    with open(f'{employee_id}.json', mode='w') as json_file:
-        json.dump(json_data, json_file, indent=4)
-
-if __name__ == '__main__':
-    # Check if an employee ID is provided as a command-line argument
+def main():
     if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
+        print("Usage: python3 2-export_to_JSON.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    save_employee_data_to_json(employee_id)
+
+    try:
+        export_to_json(employee_id)
+        print(f"Data has been exported to {employee_id}.json")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+    
